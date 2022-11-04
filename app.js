7e13babd4fce8f7cb5377d7ff097ea1d7995452a
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-11-04 10:01:09 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -42,12 +42,20 @@ app.get('/', (req, res) => {
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
 app.post('/todos', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
+
 
 
 
