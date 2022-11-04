4c818c7648e6ac009b48564eb78a91342deb79de
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-11-04 09:53:16 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 
 const Todo = require('./models/todo') // 載入 Todo model
 
@@ -25,6 +26,12 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
@@ -32,9 +39,17 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+
 
 // 設定 port 3000
 app.listen(3000, () => {
