01406005446f04f48ec2972bda9b158876140e60
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-12-05 13:22:18 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // 準備引入路由模組
 // 匯出路由器
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 module.exports = router
