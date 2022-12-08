297d3e5ca04721613c922eec6088c3a58a0bb5dc
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-12-08 16:04:17 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
-
+const usePassport = require('./config/passport')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -15,13 +15,14 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
 
+app.use(routes)
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
